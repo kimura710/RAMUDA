256e070de52e1632fc58d7dab3dfca1075a044cb
kimura <pankata79.th@gmail.com>
2020-10-14 15:00:56 +0900

    sortedの使い方

--- src/practice/Sorted.java
+++ src/practice/Sorted.java
@@ -1,5 +1,14 @@
 package practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Sorted {
+	public static void main(String[] args) {
+		Integer[] num = {1,9,54,35,71};
+		List<Integer> i = Arrays.asList(num);
+
+		i.stream().sorted((x,y) -> y - x).forEach(System.out :: println);
+	}
 
 }
